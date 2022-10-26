2cb6d47ff04c885f6a27dcc6e09546d48a60abb1
LFRS13 <894307@senacminas.edu.br>
2022-10-25 21:49:55 -0300

    commit 2

---
+++ exercicio2.py
@@ -0,0 +1,20 @@
+# Inteiros
+a = 28
+print(a)
+# Saída: 28
+
+# Real
+b = 7.5
+print(b)
+# Saída: 7.5
+
+# Caractere
+c = "ola_mundo"
+print(c)
+# Saída: ola_mundo
+
+# Boolean
+b = True
+print(b)
+# Saída: True
+
